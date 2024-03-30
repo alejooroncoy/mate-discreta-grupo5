43c730c29298b576eea6dc84a5c6364f0664b660
alejooroncoy <adoa2705@gmail.com>
2024-03-30 18:21:06 -0500

    Update all screens

--- src/screens/MenuScreen.py
+++ src/screens/MenuScreen.py
@@ -1,26 +1,36 @@
 from bases.Screen import Screen
 import customtkinter as ctk
 import sys
+from utils.fonts import getFont
+# from tkextrafont
+
 
 class MenuScreen(Screen):
     def __init__(self, parent, controller):
         super().__init__(parent, controller)
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_rowconfigure(0, weight=1)
         self.paint()
         
     def paint(self):
-        # Aquí viene
-        button_tree = ctk.CTkButton(self,text="Generar árbol");
-        button_tree.place(relx =0.5, rely=0.5, anchor=ctk.CENTER)
-        button_tree.pack(padx=10, pady=10)
+        
+        title = ctk.CTkLabel(self, text="Tree generator", font=getFont(size=50, weight="bold"  ))
+        title.pack(anchor=ctk.N, pady=60)
+        
+        containerButtons = ctk.CTkFrame(self, fg_color="transparent")
+        containerButtons.pack(side=ctk.LEFT, padx=40, anchor=ctk.N, pady=20)
+        
+        button_tree = ctk.CTkButton(containerButtons, text="Generar árbol", command=lambda: self.controller.show_screen("TreeScreen"), font=getFont(size=13), height=30)
+        button_tree.pack(pady=10)
 
-        button_instructions = ctk.CTkButton(self,text="Instrucciones");
-        button_instructions.place(relx =0.5, rely=0.4, anchor=ctk.CENTER)
-        button_instructions.pack(padx=10, pady=10)
+        
+        button_instructions = ctk.CTkButton(containerButtons, text="Instrucciones", command=lambda: self.controller.show_screen("InstructionsScreen"), font=getFont(size=13), height=30)
+        button_instructions.pack(pady=10)
 
-        button_credits = ctk.CTkButton(self,text="Créditos");
-        button_credits.place(relx =0.5, rely=0.3, anchor=ctk.CENTER)
-        button_credits.pack(padx=10, pady=10)
+        button_credits = ctk.CTkButton(containerButtons, text="Créditos", command=lambda: self.controller.show_screen("CreditsScreen"), font=getFont(size=13), height=30)
+        button_credits.pack(pady=10)
+        
 
-        button_leave = ctk.CTkButton(self,text="Salir", command=lambda: sys.exit());
-        button_leave.place(relx =0.5, rely=0.2, anchor=ctk.CENTER)
-        button_leave.pack(padx=10, pady=10)
+        button_leave = ctk.CTkButton(containerButtons, text="Salir", command=lambda: sys.exit(),  font=getFont(size=13), height=30);
+        button_leave.pack(pady=10)
+    
---
+++ src/screens/TreeScreen.py
@@ -0,0 +1,31 @@
+from bases.Screen import Screen
+import customtkinter as ctk
+import tkinter
+from utils.fonts import getFont
+
+class TreeScreen(Screen):
+    def __init__(self, parent, controller):
+        super().__init__(parent, controller)
+        self.paint()
+        
+    def paint(self):
+        label_info = ctk.CTkLabel(self, text="R={}...",font=getFont(size=20, weight="bold"))
+        label_info.pack(pady=10, padx=10)
+
+        frame_options = ctk.CTkFrame(self, fg_color="transparent")
+        frame_options.pack(side="right", expand=True, padx=0, pady=20)
+
+        btn1 = ctk.CTkButton(frame_options, text="Crear arbol binario")
+        btn1.grid(row=0, column=0, padx=10, pady=10)
+
+        btn2 = ctk.CTkButton(frame_options, text="Crear tabla \nLEFT-VALUE-RIGHT")
+        btn2.grid(row=1, column=0, padx=10, pady=10)
+
+        btn3 = ctk.CTkButton(frame_options, text="Crear nuevo arbol")
+        btn3.grid(row=2, column=0, padx=10, pady=10)
+
+        btn4 = ctk.CTkButton(frame_options, text="Regresar al menu", command=lambda: self.controller.show_screen("MenuScreen"))
+        btn4.grid(row=3, column=0, padx=10, pady=10)
+
+        canvas=tkinter.Canvas(self, width=500, height=300)
+        canvas.pack(side="right", padx=20, pady=20)
